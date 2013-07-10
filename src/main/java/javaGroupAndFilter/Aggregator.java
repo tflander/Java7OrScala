d7a6fa1e4e8b2e693e313ea4add6d0fcaf0254ba
tflanders <tflander@wowway.com>
2013-07-10 14:21:46 -0400

    more work on java group and filter

--- src/main/java/javaGroupAndFilter/Aggregator.java
+++ src/main/java/javaGroupAndFilter/Aggregator.java
@@ -8,16 +8,27 @@ import java.util.Map;
 public class Aggregator {
 
 	public static Map<String, List<Summary>> femalesByParty(List<Sample> rawData) {
-
-		List<Sample> filtered = removeMales(rawData);
-		Map<String, List<Summary>> grouped = groupByParty(filtered);
+		List<Sample> filtered = filterByField("sex", "Female", rawData);
+		Map<String, List<Summary>> grouped = groupByField("party", filtered);
+		return grouped;
+	}
+	
+	public static Map<String, List<Summary>> malesByParty(List<Sample> rawData) {
+		List<Sample> filtered = filterByField("sex", "Male", rawData);
+		Map<String, List<Summary>> grouped = groupByField("party", filtered);
+		return grouped;
+	}
+	
+	public static Map<String, List<Summary>> democratsBySex(List<Sample> rawData) {
+		List<Sample> filtered = filterByField("party", "Democrat", rawData);
+		Map<String, List<Summary>> grouped = groupByField("sex", filtered);
 		return grouped;
 	}
 
-	private static Map<String, List<Summary>> groupByParty(List<Sample> filtered) {
+	private static Map<String, List<Summary>> groupByField(String fieldName, List<Sample> filtered) {
 		HashMap<String, List<Summary>> grouped = new HashMap<String, List<Summary>>();
 		for (Sample sample : filtered) {
-			String key = sample.getParty();
+			String key = getFieldForSample(fieldName, sample);
 			List<Summary> summaryList = grouped.get(key);
 			if (summaryList == null) {
 				summaryList = new ArrayList<Summary>();
@@ -35,14 +46,25 @@ public class Aggregator {
 		return grouped;
 	}
 
-	private static List<Sample> removeMales(List<Sample> rawData) {
+	private static List<Sample> filterByField(String fieldName, String value, List<Sample> rawData) {
 		List<Sample> filtered = new ArrayList<Sample>();
 		for (Sample sample : rawData) {
-			if (sample.getSex().equals("Female")) {
+			if (getFieldForSample(fieldName, sample).equals(value)) {
 				filtered.add(sample);
 			}
 		}
 		return filtered;
 	}
-
+	
+	private static String getFieldForSample(String fieldName, Sample sample) {
+		if (fieldName.equals("sex")) {
+			return sample.getSex();
+		} else if (fieldName.equals("party")) {
+			return sample.getParty();
+		} else if (fieldName.equals("position")) {
+			return sample.getPosition();
+		}
+		throw new IllegalArgumentException("invalid sample field name " + fieldName);
+	}
+	
 }
--- src/test/java/javaGroupAndFilter/GroupAndFilterTest.java
+++ src/test/java/javaGroupAndFilter/GroupAndFilterTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class GroupAndFilterTest {
@@ -34,34 +35,54 @@ public class GroupAndFilterTest {
 		assertEquals(1, republicansAgainst.getCount());
 	}
 	
+	@Test 
+	public void itShouldSummarizeMalesByParty() {
+		List<Sample> rawData = buildSampleData();
+		Map<String, List<Summary>> summary = Aggregator.malesByParty(rawData);
+		
+		List<Summary> democrats = summary.get("Democrat");
+		List<Summary> republicans = summary.get("Republican");
+		
+		assertEquals(2, democrats.size());
+		assertEquals(2, republicans.size());
+		Summary democratsFor = democrats.get(0);
+		Summary democratsAgainst = democrats.get(1);
+		Summary republicansFor = republicans.get(0);
+		Summary republicansAgainst = republicans.get(1);
+		assertEquals("For", democratsFor.getValue());
+		assertEquals(1, democratsFor.getCount());
+		assertEquals("Against", democratsAgainst.getValue());
+		assertEquals(1, democratsAgainst.getCount());
+		assertEquals("For", republicansFor.getValue());
+		assertEquals(0, republicansFor.getCount());
+		assertEquals("Against", republicansAgainst.getValue());
+		assertEquals(2, republicansAgainst.getCount());
+	}
+	
+	@Test 
+	public void itShouldSummarizeDemocratsBySex() {
+		List<Sample> rawData = buildSampleData();
+		Map<String, List<Summary>> summary = Aggregator.democratsBySex(rawData);
+		
+		List<Summary> men = summary.get("Male");
+		List<Summary> women = summary.get("Female");
+		
+		assertEquals(2, men.size());
+		assertEquals(2, women.size());
+		Summary menFor = men.get(0);
+		Summary menAgainst = men.get(1);
+		Summary womenFor = women.get(0);
+		Summary womenAgainst = women.get(1);
+		assertEquals("For", menFor.getValue());
+		assertEquals(1, menFor.getCount());
+		assertEquals("Against", menAgainst.getValue());
+		assertEquals(1, menAgainst.getCount());
+		assertEquals("For", womenFor.getValue());
+		assertEquals(2, womenFor.getCount());
+		assertEquals("Against", womenAgainst.getValue());
+		assertEquals(0, womenAgainst.getCount());
+	}
 	/*
-
-  it("should group and filter an unordered list") {
-    val summary = Aggregator.femalesByParty(rawData)
-
-    summary.get("Democrat").head should be(Seq(
-      Summary("For", 2),
-      Summary("Against", 0)))
-    
-    summary.get("Republican").head should be(Seq(
-      Summary("For", 1),
-      Summary("Against", 1)))
-    
-  }
-  
-  it("Should summarize males by party") {
-    val summary = Aggregator.malesByParty(rawData)
-
-    summary.get("Democrat").head should be(Seq(
-      Summary("For", 1),
-      Summary("Against", 1)))
-    
-    summary.get("Republican").head should be(Seq(
-      Summary("For", 0),
-      Summary("Against", 2)))
-    
-  }
-  
   it("Should summarize Democrats by sex") {
     val summary = Aggregator.democratsBySex(rawData)
 
